f0c75db9dcdb4c1aaeb7b63f40866d7908664df9
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-09-19 06:26:01 -0300

    added the fuction section

---
+++ functions.dart
@@ -0,0 +1,2 @@
+// Dart is a true object-oriented language, so even functions are objects and have a type, Function. This means that functions can be assigned to variables or passed as arguments to other functions. You can also call an instance of a Dart class as if it were a function.
+
