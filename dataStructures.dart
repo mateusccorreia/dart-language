337874ba378d4863efc51a6a1035e2a478728d35
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-09-28 05:44:55 -0300

    added the content about maps and sets

--- dataStructures.dart
+++ dataStructures.dart
@@ -43,4 +43,94 @@ void main() {
   print(values);
   values.removeAt(6);
   print(values);
+
+  List<String> times = ['Palmeiras', 'Fortaleza', 'Ceará'];
+  
+  var any = times.iterator;
+  while(any.moveNext()) {
+    print(any.current);
+  }
+
+  times.shuffle();
+  print(times);
+
+  //  Creating an empty map
+
+  //Via constructor:
+  var map = Map();
+  print(map);
+
+  //Via its literals:
+  var mapOne = {"name": "Souza", "age": "32"};
+  mapOne["name"] = "Mateus";
+  mapOne["age"] = "33";
+  print(mapOne);
+  mapOne.addAll({"school": "semicolon", "degree": "Bsc"});
+  print(mapOne);
+  mapOne.putIfAbsent("adress", () => "rua são francisco");
+  print(mapOne);
+  mapOne["time"] = "Palmeiras";
+  print(mapOne);
+
+  //Updating an entry in a map
+  print(mapOne["time"]);
+  mapOne["age"] = "32";
+  print(mapOne);
+
+  //Deleting an entry from a map
+  var num = {
+    'ten': 10,
+    'eleven': 11,
+    'twelve': 12,
+    'thirteen': 13,
+    'fourteen': 14,
+    'fifteen': 15,
+    'sixteen': 16
+  };
+
+  num.remove('ten');
+  print(num);
+
+  // remove all entries with even number values
+  num.removeWhere((key, value) => value % 2 == 0);
+  print(num);
+
+  //Creating a map to contain specific data types
+  //You can create a map that contains the keys and values of defined datatypes. For instance, you may want your map to contain keys of the String datatype and int values. Below is an example of how you can go about this:
+
+  Map<String, int> novoMap = {'first': 10, 'second': 20, 'third': 30, 'fourth': 40};
+
+  novoMap['fifth'] = 50;
+  print(novoMap);
+
+  // defining the data types via the constructor
+  var segundoMap = Map<String, int>();
+  segundoMap['name'] = 5;
+  print(segundoMap);
+
+  //Iterating through a map
+  // iterating through the keys of the map using the for...in loop to print all the values 
+
+  for(String k in novoMap.keys) {
+    print(novoMap[k]);
+  }
+
+  //In the for...in loop example above, we iterated through the keys in the map. For each iteration, we fetched each key, stored it in the variable k, and used it to print the values in the map.
+
+  //The forEach() method takes a two-argument function, just as we described with the removeWhere() method. Using the forEach() method allows you to perform operations on both the keys and the values simultaneously.
+
+  novoMap.forEach((key, value) { 
+    print("key = $key, value = $value");
+  });
+
+  //Flutter data structures: Set
+  //A set is a collection of unique items. Unlike lists, sets are particularly unique data structures that ensure duplicate values do not exist in the collection.
+
+  Set primeiroSet = {1, 2, 3, 4, 5};
+  Set segundoSet = {4, 5};
+  print(primeiroSet.intersection(segundoSet));
+
+  // Sets are best suited for storing unique values whose order is not essential as a variable. Below are three types of sets we’ll look at in detail:
+
+  //HashSet, LinkedHashSet, SplayTreeSet
 }
